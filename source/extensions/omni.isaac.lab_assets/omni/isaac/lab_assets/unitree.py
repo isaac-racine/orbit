2c6435197490b413287d5ce64b0a91ebfa337eed
isaac-racine <raci2203@usherbrooke.ca>
2024-09-09 14:33:47 +0900

    adding the go2z1 assembly asset configuration

--- source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/unitree.py
+++ source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/unitree.py
@@ -176,6 +176,63 @@ UNITREE_GO2_CFG = ArticulationCfg(
 )
 """Configuration of Unitree Go2 using DC-Motor actuator model."""
 
+UNITREE_GO2_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"/home/isaac/Omniverse/IsaacLab/Quadruped_Model/Unitree/go2z1.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False, solver_position_iteration_count=4, solver_velocity_iteration_count=0
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.4),
+        joint_pos={
+            ".*L_hip_joint": 0.1,
+            ".*R_hip_joint": -0.1,
+            "F[L,R]_thigh_joint": 0.8,
+            "R[L,R]_thigh_joint": 1.0,
+            ".*_calf_joint": -1.5,
+            "joint1" 0.0,
+            "joint2" 0.0,
+            "joint3" 0.0,
+            "joint4" 0.0,
+            "joint5" 0.0,
+            "joint6" 0.0,
+            "jointGripper" 0.0,
+        },
+        joint_vel={".*": 0.0},
+    ),
+    soft_joint_pos_limit_factor=0.9,
+    actuators={
+        "base_legs": DCMotorCfg(
+            joint_names_expr=[".*_hip_joint", ".*_thigh_joint", ".*_calf_joint"],
+            effort_limit=23.5,
+            saturation_effort=23.5,
+            velocity_limit=30.0,
+            stiffness=25.0,
+            damping=0.5,
+            friction=0.0,
+        ),
+        "base_arm": ImplicitActuatorCfg(
+            joint_names_expr=["joint.*"],
+            # all parameters use USD values
+			effort_limit=None,velocity_limit=None,
+            friction=None,armature=None,
+            #stiffness=None,damping=None,
+            stiffness=25.0, damping=0.5,
+    },
+)
+"""Configuration of Unitree Go2 using DC-Motor actuator model."""
+
 
 H1_CFG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
